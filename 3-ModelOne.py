ac12c6997942c760ba808323351ddcf43fd8e6a4
JonahDinh <jonahdinh@hotmail.com>
2024-05-21 13:06:12 -0600

    Housing prediction

--- 3-ModelOne.py
+++ 3-ModelOne.py
@@ -1,7 +1,29 @@
-import pandas as pd
-import numpy as np
-import seaborn as sb
+import keras
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras import layers
+
+
+def acc_chart(results):
+    plt.title("Accuracy of model")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epochs")
+    plt.plot(results.history['accuracy'])
+    plt.plot(results.history['val_accuracy'])
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+
+def loss_chart(results):
+    plt.title("Model losses")
+    plt.ylabel("Loss")
+    plt.xlabel("Epochs")
+    plt.plot(results.history['loss'])
+    plt.plot(results.history['val_loss'])
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
 
 df = pd.read_csv("Data/kc_house_data.csv")
 
@@ -48,6 +70,42 @@ df.drop(['id', 'zipcode', 'lat', 'long'], axis=1, inplace=True)
 
 df = df[df['house_age'] >= 0]
 
-for i in df.columns:
-    sb.displot(df[i])
-    plt.show()
+# for i in df.columns:
+#     sb.displot(df[i])
+#     plt.show()
+
+
+# sb.pairplot(df['price', 'bedroom'])
+# plt.show()
+
+# The heat map can be used to show correlation between different series
+# plt.figure(figsize=(30, 20))
+# sb.heatmap(df.corr(), annot=True)
+# plt.show()
+
+# We want to be able to predict the price of a house based upon
+# other series values. So we are going to create a dataframe for our input values
+# and another datafram for our output values, both of these become part of our model
+# andalysis later on.
+
+X = df.drop("price", axis=1)
+Y = df['price']
+
+print(X.head().to_string())
+print(Y.head().to_string())
+
+print(X.shape)
+print(Y.shape)
+
+# Define our model
+# We are going to have one input layer, one hidden layer and 1 output layer.
+
+my_model = keras.Sequential()
+my_model.add(layers.Dense(14, activation="relu"))
+my_model.add(layers.Dense(4, activation="relu"))
+my_model.add(layers.Dense(1))
+
+# Compile our model & put these into a results
+
+my_model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+results = my_model.fit(X, Y, validation_split=0.33, batch_size=64, epochs=10)
